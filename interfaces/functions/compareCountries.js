10f3e65f5387848b7797ac72276487c433eb8ccd
lds <lucjan.ds@gmail.com>
2016-07-27 19:02:50 +0200

    debugging

---
+++ interfaces/functions/compareCountries.js
@@ -0,0 +1,7 @@
+compareCountries = function(activity, value) {
+    var country;
+    var offset = 9999;
+    dataSet.forEach(function(instance) {
+        if(Math.abstimeToMinutes(instance[activity]) - timeToMinutes(value))
+    });
+} 
