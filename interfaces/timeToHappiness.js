d0fb576cc28c5f477b144e1403fab80ee3152030
marco.dussin <marco@duss.in>
2016-07-27 18:40:07 +0200

    timeTohappiness function

---
+++ interfaces/timeToHappiness.js
@@ -0,0 +1,38 @@
+(function($){
+  $.fn.extend({
+    timeToHappiness: function(category, time) {
+
+      //var options = $.extend(defaults, options);
+
+      //console.log(dataSet);
+
+      var sortedDataSet = _.sortBy(dataSet, function(item) { return timeToMinutes(item[category]) });
+
+      var myArray=[];
+
+      sortedDataSet.forEach(function (item){
+        myArray.push(timeToMinutes(item[category]));
+      })
+
+      var myTime=timeToMinutes(time);
+
+      function closest(list, x) {
+        var min,
+          chosen = 0;
+        for (var i in list) {
+          min = Math.abs(list[chosen] - x);
+          if (Math.abs(list[i] - x) < min) {
+            chosen = i;
+          }
+        }
+        return chosen;
+      }
+
+
+      console.log(minutesToTime(myArray[closest(myArray, myTime)]));
+
+      return sortedDataSet[closest(myArray, myTime)]['Happiness score'];
+
+    }
+  });
+})(jQuery);
